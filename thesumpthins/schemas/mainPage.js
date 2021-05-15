95c131fcdb59e6f3816a783f90adf1c34f5614c5
George Hart <hamiltonhart@protonmail.com>
2021-05-14 20:21:34 -0700

    Started work on Sanity schemas

---
+++ thesumpthins/schemas/mainPage.js
@@ -0,0 +1,12 @@
+export default {
+  name: "mainPageText",
+  title: "Main Page Text",
+  type: "document",
+  fields: [
+    {
+      name: "Text",
+      title: "Text",
+      type: "blockContent",
+    },
+  ],
+};
