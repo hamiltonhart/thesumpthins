7c35b911fa8ef12bd4ffbbaa6d845d383d35c8ae
George Hart <hamiltonhart@protonmail.com>
2021-06-18 13:25:39 -0700

    Adjusted api/contact.js to hopefully fix the Oauth issue.

--- frontend/pages/api/contact.js
+++ frontend/pages/api/contact.js
@@ -1,33 +1,33 @@
 export default async function (req, res) {
   const nodemailer = require("nodemailer");
-  // const { google } = require("googleapis");
-  // const OAuth2 = google.auth.OAuth2;
+  const { google } = require("googleapis");
+  const OAuth2 = google.auth.OAuth2;
 
-  // const oauth2Client = new OAuth2(
-  //   process.env.CLIENT_ID,
-  //   process.env.CLIENT_SECRET,
-  //   "https://developers.google.com/oauthplayground"
-  // );
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
 
-  // oauth2Client.setCredentials({
-  //   refresh_token: process.env.REFRESH_TOKEN,
-  // });
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
 
-  // const accessToken = await new Promise((resolve, reject) => {
-  //   oauth2Client.getAccessToken((err, token) => {
-  //     if (err) {
-  //       reject(`Failed to create access token ${err}`);
-  //     }
-  //     resolve(token);
-  //   });
-  // });
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject(`Failed to create access token -- ${err}`);
+      }
+      resolve(token);
+    });
+  });
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
       user: process.env.EMAIL_ADDRESS,
-      // accessToken,
+      accessToken,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       refreshToken: process.env.REFRESH_TOKEN,
@@ -35,7 +35,7 @@ export default async function (req, res) {
   });
 
   try {
-    const info = await transporter.sendMail({
+    await transporter.sendMail({
       from: `<${req.body.email}>${req.body.name}`,
       to: process.env.EMAIL_ADDRESS,
       subject: `Contact Form: ${req.body.subject}`,
