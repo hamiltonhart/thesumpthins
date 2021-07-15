b563021a796cb32fb34b433df001f8e427d4f374
George Hart <hamiltonhart@protonmail.com>
2021-07-14 22:39:28 -0700

    Adjusted nodemailer.

--- frontend/pages/api/contact.js
+++ frontend/pages/api/contact.js
@@ -1,36 +1,32 @@
 export default async function (req, res) {
   const nodemailer = require("nodemailer");
-  const { google } = require("googleapis");
-  const OAuth2 = google.auth.OAuth2;
+  // const { google } = require("googleapis");
+  // const OAuth2 = google.auth.OAuth2;
 
-  const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
+  // const oauth2Client = new OAuth2(
+  //   process.env.CLIENT_ID,
+  //   process.env.CLIENT_SECRET,
+  //   "https://developers.google.com/oauthplayground"
+  // );
 
-  oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-  });
+  // oauth2Client.setCredentials({
+  //   refresh_token: process.env.REFRESH_TOKEN,
+  // });
 
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject(`Failed to create access token -- ${err}`);
-      }
-      resolve(token);
-    });
-  });
+  // const accessToken = await new Promise((resolve, reject) => {
+  //   oauth2Client.getAccessToken((err, token) => {
+  //     if (err) {
+  //       reject(`Failed to create access token -- ${err}`);
+  //     }
+  //     resolve(token);
+  //   });
+  // });
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      type: "OAuth2",
       user: process.env.EMAIL_ADDRESS,
-      accessToken,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
